441fab90aab2a0967fbb0e67ad983a3b23790cc8
Vladislav Revucki <vlad-revuckii@mail.ru>
2025-06-22 18:02:51 +0300

    Header and Button

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,13 +1,14 @@
 import {} from "react"
 import cls from "./MainLayout.module.css"
 import { Outlet } from "react-router"
+import { Header } from "../Header"
 
 export const MainLayout = () => {
 	const currentYear = new Date().getFullYear()
 
 	return (
 		<div className={cls.mainLayout}>
-			<header>header</header>
+			<Header />
 			<div className={cls.mainWrapper}>
 				<main className={cls.main}>
 					<Outlet />
