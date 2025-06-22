edf83b7059029b44026f5853bb8629327274779c
Vladislav Revucki <vlad-revuckii@mail.ru>
2025-06-22 19:48:43 +0300

    NotFoundPage, QuestionCard props, dynemic routes

--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -1,24 +1,31 @@
 import {} from "react"
 import cls from "./QuestionCard.module.css"
 import { Button } from "../Button"
+import { useNavigate } from "react-router-dom"
 
-export const QuestionCard = () => {
+export const QuestionCard = ({ card }) => {
+	const navigate = useNavigate()
+
 	return (
 		<div className={cls.card}>
 			<div className={cls.cardLabels}>
-				<div>Level: 1</div>
-				<div>Not Completed</div>
+				<div>Level: {card.level}</div>
+				<div>{card.completed ? "Выполнен" : "Не выполнен"}</div>
 			</div>
-			<h5 className={cls.cardTitle}>Что такое JSX?</h5>
+			<h5 className={cls.cardTitle}>{card.question}</h5>
 
 			<div className={cls.cardAnswers}>
 				<label>short answer:</label>
-				<p className={cls.cardAnswer}>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, delectus.
-				</p>
+				<p className={cls.cardAnswer}>{card.answer}</p>
 			</div>
 
-			<Button onClick={() => {}}>Показать</Button>
+			<Button
+				onClick={() => {
+					navigate(`/question/${card.id}`)
+				}}
+			>
+				Показать
+			</Button>
 		</div>
 	)
 }
