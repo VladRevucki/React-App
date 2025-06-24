b7a3d0f340e34b1ad1c3059f1f41a016237e0cd5
Vladislav Revucki <vlad-revuckii@mail.ru>
2025-06-24 12:28:35 +0300

    useFetch, delayFn

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,32 +4,50 @@ import { QuestionCard } from "../../components/QuestionCard"
 import { API_URL } from "../../constants"
 import { QuestionCardList } from "../../components/QuestionCardList"
 import { Loader } from "../../components/Loader"
+import { delayFn } from "../../helpers/delayFn"
+import { useFetch } from "../../hooks/useFetch"
 
 export const HomePage = () => {
 	const [questions, setQuestions] = useState([])
+	/**добавление события для лоадера */
+	// const [isLoading, setIsLoading] = useState(false)
 
-	const getQuestions = async () => {
-		try {
-			const response = await fetch(`${API_URL}/react`)
-			const questions = await response.json()
+	const [getQuestions, isLoading, error] = useFetch(async url => {
+		const response = await fetch(`${API_URL}/${url}`)
+		const questions = await response.json()
 
-			setQuestions(questions)
+		setQuestions(questions)
+		return questions
+	})
 
-			console.log("questions", questions)
-		} catch (error) {
-			console.error(error)
-		}
-	}
+	// const _getQuestions = async () => {
+	// 	try {
+	// 		setIsLoading(true) /**запуск лоадера */
+	// 		await delayFn() /**отображение асинхр.функции запуска лоадера */
+
+	// 		const response = await fetch(`${API_URL}/react`)
+	// 		const questions = await response.json()
+
+	// 		setQuestions(questions)
+
+	// 		console.log("questions", questions)
+	// 	} catch (error) {
+	// 		console.error(error)
+	// 	} finally {
+	// 		setIsLoading(false)
+	// 	}
+	// }
 
 	useEffect(() => {
-		getQuestions()
+		getQuestions("react")
 	}, []) /**если пустой массив, отработает один раз, когда отработает компонент */
 
 	/**	getQuestions() если так вызывать функцию, чтобы элементы отображались при загрузке страницы, то будет бесконечный рендеринг */
 
 	return (
 		<>
-			<Loader />
+			{isLoading && <Loader />}
+			{error && <p>{error}</p>}
 			<QuestionCardList cards={questions} />
 		</>
 	)
