772bf6f1a67a3676ca4dee911e220cfc390bd5d6
Vladislav Revucki <vlad-revuckii@mail.ru>
2025-06-25 13:30:49 +0300

    pagination, scroll logic

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useMemo, useRef, useState } from "react"
 import cls from "./HomePage.module.css"
 import { QuestionCard } from "../../components/QuestionCard"
 import { API_URL } from "../../constants"
@@ -6,15 +6,29 @@ import { QuestionCardList } from "../../components/QuestionCardList"
 import { Loader } from "../../components/Loader"
 import { useFetch } from "../../hooks/useFetch"
 import { SearchInput } from "../../components/SearchInput"
+import { Button } from "../../components/Button"
+
+const DEFAULT_PER_PAGE = 10
 
 export const HomePage = () => {
-	const [questions, setQuestions] = useState([])
+	const [questions, setQuestions] = useState({})
+
+	const [searchParams, setSearchParams] = useState(
+		`?_page=1&_per_page=${DEFAULT_PER_PAGE}`
+	)
 	/**добавление события для лоадера */
 	// const [isLoading, setIsLoading] = useState(false)
 
 	const [searchValue, setSearchValue] = useState("")
 
 	const [sortSelectValue, setSortSelectValue] = useState("")
+
+	const controlsContainerRef = useRef()
+
+	/**функция для выделения кнопки пагинации */
+	const getActivePageNumber = () =>
+		questions.next === null ? questions.last : questions.next - 1
+	///
 
 	const [getQuestions, isLoading, error] = useFetch(async url => {
 		const response = await fetch(`${API_URL}/${url}`)
@@ -41,28 +55,63 @@ export const HomePage = () => {
 	// 		setIsLoading(false)
 	// 	}
 	// }
-	const cards = questions.filter(d =>
-		d.question.toLowerCase().includes(searchValue.trim().toLowerCase())
-	)
+
+	/**поиск элементов по инпуту */
+	const cards = useMemo(() => {
+		if (questions?.data) {
+			if (searchValue.trim()) {
+				return questions.data.filter(d =>
+					d.question.toLowerCase().includes(searchValue.trim().toLowerCase())
+				)
+			} else {
+				return questions.data
+			}
+		}
+		/**если же data нет, то возвращаем пустой массив */
+		return []
+	}, [questions, searchValue])
+
+	/**отображение количества карточек на странице */
+	const pagination = useMemo(() => {
+		const totalCardsCount = questions?.pages || 0
+
+		/**формируем массив из полученного количества страниц */
+		return Array(totalCardsCount)
+			.fill(0)
+			.map((_, i) => i + 1)
+	}, [questions])
 
 	useEffect(() => {
-		getQuestions(`react?${sortSelectValue}`)
+		getQuestions(`react${searchParams}`)
 	}, [
-		sortSelectValue,
+		searchParams,
 	]) /**если пустой массив, отработает один раз, когда отработает компонент */
 
 	/**	getQuestions() если так вызывать функцию, чтобы элементы отображались при загрузке страницы, то будет бесконечный рендеринг */
 
 	const onSortSelectChangeHandler = e => {
 		setSortSelectValue(e.target.value)
+
+		setSearchParams(`?_page=1&_per_page=${DEFAULT_PER_PAGE}&${e.target.value}`)
 	}
+
 	const onSearchChangeHandler = e => {
 		setSearchValue(e.target.value)
 	}
 
+	const paginationHandler = e => {
+		if (e.target.tagName === "BUTTON") {
+			setSearchParams(
+				`?_page=${e.target.textContent}&_per_page=${DEFAULT_PER_PAGE}&${sortSelectValue}`
+			)
+			/**для скролла вверх при клике на кнопку пагинации */
+			controlsContainerRef.current.scrollIntoView({ behavior: "smooth" })
+		}
+	}
+
 	return (
 		<>
-			<div className={cls.controlsContainer}>
+			<div className={cls.controlsContainer} ref={controlsContainerRef}>
 				<SearchInput value={searchValue} onChange={onSearchChangeHandler} />
 				<select
 					value={sortSelectValue}
@@ -80,7 +129,20 @@ export const HomePage = () => {
 			{isLoading && <Loader />}
 			{error && <p>{error}</p>}
 			<QuestionCardList cards={cards} />
-			{cards.length === 0 && <p className={cls.noCardsInfo}>Не найдено...</p>}
+
+			{cards.length === 0 ? (
+				<p className={cls.noCardsInfo}>Не найдено...</p>
+			) : (
+				<div className={cls.paginationContainer} onClick={paginationHandler}>
+					{pagination.map(value => {
+						return (
+							<Button key={value} isActive={value === getActivePageNumber()}>
+								{value}
+							</Button>
+						)
+					})}
+				</div>
+			)}
 		</>
 	)
 }
