866c4cd815905974970f69ea0b94aab8edb8bd32
Vladislav Revucki <vlad-revuckii@mail.ru>
2025-06-26 10:08:12 +0300

    react toasts

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -2,22 +2,26 @@ import {} from "react"
 import cls from "./MainLayout.module.css"
 import { Outlet } from "react-router"
 import { Header } from "../Header"
+import { ToastContainer } from "react-toastify"
 
 export const MainLayout = () => {
 	const currentYear = new Date().getFullYear()
 
 	return (
-		<div className={cls.mainLayout}>
-			<Header />
-			<div className={cls.mainWrapper}>
-				<main className={cls.main}>
-					<Outlet />
-				</main>
-				<footer className={cls.footer}>
-					React Question Card Application | {currentYear} <br />
-					by Vladislav Revutski
-				</footer>
+		<>
+			<div className={cls.mainLayout}>
+				<Header />
+				<div className={cls.mainWrapper}>
+					<main className={cls.main}>
+						<Outlet />
+					</main>
+					<footer className={cls.footer}>
+						React Question Card Application | {currentYear} <br />
+						by Vladislav Revutski
+					</footer>
+				</div>
 			</div>
-		</div>
+			<ToastContainer />
+		</>
 	)
 }
