cfbdace5e07032532599b8a7a310b18ce9ff6613
Vladislav Revucki <vlad-revuckii@mail.ru>
2025-06-26 16:56:45 +0300

    question form

--- src/pages/AddQuestionsPage/AddQuestionsPage.jsx
+++ src/pages/AddQuestionsPage/AddQuestionsPage.jsx
@@ -5,6 +5,7 @@ import { delayFn } from "../../helpers/delayFn"
 import { toast } from "react-toastify"
 import { API_URL } from "../../constants"
 import { Loader } from "../../components/Loader"
+import { QuestionForm } from "../../components/QuestionForm"
 
 const createCardAction = async (_prevState, formData) => {
 	try {
@@ -51,87 +52,12 @@ const AddQuestionsPage = () => {
 
 			<h1 className={cls.formTitle}>Добавить новый вопрос</h1>
 			<div className={cls.formContainer}>
-				<form action={formAction} className={cls.form}>
-					<div className={cls.formControl}>
-						<label htmlFor="questionField">Вопрос:</label>
-						<textarea
-							defaultValue={formState.question}
-							name="question"
-							id="questionField"
-							cols="30"
-							rows="2"
-							required
-							placeholder="пожалуйста введите вопрос"
-						></textarea>
-					</div>
-
-					<div className={cls.formControl}>
-						<label htmlFor="answerField">Короткий ответ:</label>
-						<textarea
-							defaultValue={formState.answer}
-							name="answer"
-							id="answerField"
-							cols="30"
-							rows="2"
-							required
-							placeholder="пожалуйста введите короткий ответ"
-						></textarea>
-					</div>
-
-					<div className={cls.formControl}>
-						<label htmlFor="descriptionField">Описание:</label>
-						<textarea
-							defaultValue={formState.description}
-							name="description"
-							id="descriptionField"
-							cols="30"
-							rows="5"
-							required
-							placeholder="пожалуйста введите полное описание"
-						></textarea>
-					</div>
-
-					<div className={cls.formControl}>
-						<label htmlFor="resourcesField">Ресурсы:</label>
-						<textarea
-							defaultValue={formState.resources}
-							name="resources"
-							id="resourcesField"
-							cols="30"
-							rows="5"
-							placeholder="пожалуйста введите ресурсы, разделенные запятыми"
-						></textarea>
-					</div>
-
-					<div className={cls.formControl}>
-						<label htmlFor="levelField">Уровень:</label>
-						<select
-							defaultValue={formState.level}
-							name="level"
-							id="levelField"
-							className={cls.select}
-						>
-							<option disabled>Уровень вопроса</option>
-							<hr />
-							<option value="1">1 - легкий</option>
-							<option value="2">2 - средний</option>
-							<option value="3">3 - сложный</option>
-						</select>
-					</div>
-
-					<label htmlFor="clearFormControl" className={cls.clearFormControl}>
-						<input
-							type="checkbox"
-							name="clearForm"
-							id="clearFormControl"
-							defaultChecked={formState.clearForm}
-							className={cls.checkbox}
-						/>
-						<span>очистить форму</span>
-					</label>
-
-					<Button isDisabled={isPending}>Добавить вопрос</Button>
-				</form>
+				<QuestionForm
+					state={formState}
+					formAction={formAction}
+					isPending={isPending}
+					submitBtnText={"Добавить вопрос"}
+				/>
 			</div>
 		</>
 	)
